5bc826c5439d4c5fa28ee1d2c714411b775d20e5
James Nixon <69281179+JamesNixon94@users.noreply.github.com>
2020-09-09 21:41:38 +0100

    Update to code so 'hello world...' page has been deleted.

--- vsearch4web/vsearch4web.py
+++ vsearch4web/vsearch4web.py
@@ -3,9 +3,7 @@ from vsearch import search4letters
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello() ->str:
-    return 'Hello world from flask!'
+
 
 @app.route('/search4', methods = ['POST'])
 def do_search() -> 'html':
@@ -15,8 +13,9 @@ def do_search() -> 'html':
     results = str(search4letters(phrase, letters))
     return render_template('results.html', the_phrase = phrase, the_letters = letters, the_title = title, the_results = results,)
 
+@app.route('/')
 @app.route('/entry')
 def entry_page() -> 'html':
-    return render_template('entry.html', the_title = 'Welcome to search4letters one the web!')
+    return render_template('entry.html', the_title = 'Welcome to search4letters on the web!')
 
-app.run(debug = True)
+app.run()
